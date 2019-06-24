87fb2c669bcce0f231b1834c347a09e3be48778f
Jake Esveld <thejakeesveld@gmail.com>
2019-06-24 10:43:20 -0700

    Initial commit

---
+++ src/main/java/com/cep/corporateeventplanner/CorporateeventplannerApplication.java
@@ -0,0 +1,13 @@
+package com.cep.corporateeventplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateeventplannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorporateeventplannerApplication.class, args);
+    }
+
+}
