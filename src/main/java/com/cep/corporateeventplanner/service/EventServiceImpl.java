2c031d497659e4cd371e71dddbc1efdf1eafca0c
Jake Esveld <thejakeesveld@gmail.com>
2019-06-24 14:26:13 -0700

    Create Event and Task basic Repos, and create basic Services for event and task

---
+++ src/main/java/com/cep/corporateeventplanner/service/EventServiceImpl.java
@@ -0,0 +1,37 @@
+package com.cep.corporateeventplanner.service;
+
+import com.cep.corporateeventplanner.model.Event;
+
+import java.util.List;
+
+public class EventServiceImpl implements EventService {
+    @Override
+    public List<Event> findAll() {
+        return null;
+    }
+
+    @Override
+    public Event findById(long id) {
+        return null;
+    }
+
+    @Override
+    public Event findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Event create(Event event) {
+        return null;
+    }
+
+    @Override
+    public void updateEvent(Event event, long id) {
+
+    }
+
+    @Override
+    public void deleteEvent(long id) {
+
+    }
+}
