c89968d479d4cd718c41a067cbf24e1ffee10417
Jake Esveld <thejakeesveld@gmail.com>
2019-06-24 15:29:53 -0700

    Add basic functionality to event service

--- src/main/java/com/cep/corporateeventplanner/repo/EventRepository.java
+++ src/main/java/com/cep/corporateeventplanner/repo/EventRepository.java
@@ -4,4 +4,5 @@ import com.cep.corporateeventplanner.model.Event;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EventRepository extends CrudRepository<Event, Long> {
+    Event findByName(String name);
 }
--- src/main/java/com/cep/corporateeventplanner/service/EventServiceImpl.java
+++ src/main/java/com/cep/corporateeventplanner/service/EventServiceImpl.java
@@ -1,39 +1,83 @@
 package com.cep.corporateeventplanner.service;
 
 import com.cep.corporateeventplanner.model.Event;
+import com.cep.corporateeventplanner.model.Task;
+import com.cep.corporateeventplanner.model.User;
+import com.cep.corporateeventplanner.repo.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service(value = "eventService")
 public class EventServiceImpl implements EventService {
+
+    @Autowired
+    EventRepository repo;
+
     @Override
     public List<Event> findAll() {
-        return null;
+        List<Event> eventList = new ArrayList<>();
+        repo.findAll().iterator().forEachRemaining(eventList::add);
+        return eventList;
     }
 
     @Override
     public Event findById(long id) {
-        return null;
+        return repo.findById(id).orElseThrow(EntityNotFoundException::new);
     }
 
     @Override
     public Event findByName(String name) {
-        return null;
+        return repo.findByName(name);
     }
 
     @Override
     public Event create(Event event) {
-        return null;
+        repo.save(event);
+        return event;
     }
 
     @Override
     public void updateEvent(Event event, long id) {
+        Event currentEvent = repo.findById(id).orElseThrow(EntityNotFoundException::new);
+        if (event.getBudget() != null){
+            currentEvent.setBudget(event.getBudget());
+        }
+        if (event.getCompanyname() != null){
+            currentEvent.setCompanyname(event.getCompanyname());
+        }
+        if (event.getDate() != null){
+            currentEvent.setDate(event.getDate());
+        }
+        if (event.getDescription() != null){
+            currentEvent.setDescription(event.getDescription());
+        }
+        if(event.getName() != null){
+            currentEvent.setName(event.getName());
+        }
+        if(event.getTasklist() != null && event.getTasklist().size() > 0){
+            for (Task task: event.getTasklist()){
+                currentEvent.getTasklist().add(task);
+            }
+        }
+        if(event.getUserlist() != null && event.getUserlist().size() > 0){
+            for (User user: event.getUserlist()){
+                currentEvent.getUserlist().add(user);
+            }
+        }
+        repo.save(currentEvent);
 
     }
 
     @Override
     public void deleteEvent(long id) {
-
+        if (repo.findById(id).isPresent()){
+            repo.deleteById(id);
+        }else{
+            throw new EntityNotFoundException();
+        }
     }
 }
